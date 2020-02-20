cbe3bbf750d99e95a508892b071d4fe03ccd6198
WENDERSON SILVEIRA <WSO.SILVER@GMAIL.COM>
2020-02-20 13:18:58 -0300

    Ciração de usuarios

---
+++ src/controllers/authController.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const User = require('../models/user');
+const router = express.Router();
+
+router.post('/register', async (req, res) =>{
+    const { email } = req.body;
+
+    try{    
+    if (await User.findOne({ email })){
+    return res.status(400).send({ error: 'User already exists '})
+    }
+
+    else{
+    return 
+        const user = await User.create(req.body);
+
+
+        user.password = undefined;
+
+
+        return res.send({ user });
+    }   
+
+    } catch (erro){
+        return res.status(400).send({ error: 'Registration failed'})
+    }
+})
+
+
+module.exports = app => app.use('/auth', router);
+
+
